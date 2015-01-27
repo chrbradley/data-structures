3a97ace714dedc447a533efd5a8946b47ccc9465
Chris Bradley <chrbradley@gmail.com>
2015-01-27 14:39:00 -0500

    implmented set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*

569a633d3123cc166f8ab9ec676632dc3ae96969
Chris Bradley <chrbradley@gmail.com>
2015-01-27 14:06:02 -0500

    implemented linked list and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newTail = makeNode(value);
+    if (list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    }
+    var oldTail = list.tail;
+    list.tail = newTail;
+    oldTail.next = list.tail;
   };
 
   list.removeHead = function(){
+    var oldHead = list.head;
+    var newHead = list.head.next;
+    list.head = newHead;
+    return oldHead.value;
   };
 
   list.contains = function(target){
+
+    var checkValue = function (node) {
+      node = node || this;
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return checkValue(node.next);
+      }
+    };
+    return checkValue(this.head);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,51 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
+
   return newTree;
 };
 
 
 
 
-var treeMethods = {};
+var treeMethods = {
+  addChild: function(value){
+    var newChild = makeTree(value);
+    this.children.push(newChild);
+  },
 
-treeMethods.addChild = function(value){
+  contains: function(target){
+    var checkTarget = function(node) {
+      console.log(node.value);
+      if (node.value === target) {
+        return true;
+      } else if (node.children.length !== 0) {
+        for (var i = 0; i < node.children.length; i++) {
+          if (checkTarget(node.children[i])) {
+            return true;
+          }
+        }
+      }
+      return false;
+    };
+    return checkTarget(this);
+  }
 
 };
 
-treeMethods.contains = function(target){
-
-};
-
-
+// var tree = makeTree(1);
+// tree.addChild('a');
+// tree.addChild('b');
+// tree.addChild('c');
+// tree.children[0].addChild(2);
+// tree.children[0].addChild(4);
+// tree.children[0].addChild(6);
+// tree.children[2].addChild(3);
+// tree.children[2].addChild(5);
+// tree.children[2].addChild(7);
 /*
  * Complexity: What is the time complexity of the above functions?
  */
